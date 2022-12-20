e618504a19ffc0a05d2e1cc9dcc5da8dcda7d08d
Nicholas Molina <nicholas.molina.t@gmail.com>
2022-12-20 01:06:29 -0300

    probando nombre

--- gear2/src/App.js
+++ gear2/src/App.js
@@ -10,25 +10,41 @@ class App extends Component{
     productos: [
       { name: 'Caja sorpresa', price: 1500, img: '/productos/box.jpg'},
       { name: 'Luffytaro', price: 1500, img: '/productos/funko.jpg'},
-      { name: 'Monopoly', price: 1500, img: '/productos/Monopoly.jpg'},
-      
-    ]
+      { name: 'Monopoly', price: 1500, img: '/productos/Monopoly.jpg'},    
+    ],
+    carro:[ ],
   }
 
+  agregarAlCarro = (producto) => {
+    const { carro } = this.state
+    if (carro.find(x => x.name === producto.name)) {
+      const newCarro = carro.map(x => x.name === producto.name
+        ? ({
+          ...x,
+          cantidad: x.cantidad + 1
+        })
+        : x)
+        return this.setState({ carro: newCarro})
+    }
+    return this.setState({
+      carro: this.state.carro.concat({
+        ...producto,
+        cantidad: 1,
+      })
+    })
+  }
 
   render() {
-    return(
-      <div>
-        <Navbar/>
+    return(  
+      <div>  
+        <Navbar carro={this.state.carro}/>
         <Layout>
           <Title/>
           <Productos 
-            agregarAlCarro={() => console.log('No hace nadi')}
+            agregarAlCarro={this.agregarAlCarro}
             productos={this.state.productos}>
           </Productos>
         </Layout>
-        
-      
       </div>
     )
   }
